218801c7c960194bf90fb2ebffbbf1ceee0dcd1d
Chi-naza <ugwuokechinazajames@gmail.com>
2024-05-21 13:33:19 +0100

    Initial Commit

---
+++ eCommerceApp/eCommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceAppApp.swift
+//  eCommerceApp
+//
+//  Created by Chinaza Ugwuoke on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
